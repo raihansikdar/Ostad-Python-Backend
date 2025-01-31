713d08ba873bd943f8e7731e1df3f38eeb2a54a6
raihan sikdar <raihansikdar10@gmail.com>
2025-01-31 23:16:55 +0600

    java script

---
+++ module-12/script.js
@@ -0,0 +1,12 @@
+document.write("Hello Raihan");
+document.write("<br>");  
+document.write("Hello Sinthy");
+document.write("<br>");
+
+let age = 25;
+
+if(age >= 18){
+    document.write("You are adult");
+}else{
+    document.write("You are not adult");
+}
